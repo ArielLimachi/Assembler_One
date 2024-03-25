929d974fc130a461ed639b54391a83eff23e14fe
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2024-03-25 00:44:12 -0400

    add logic of the inputs

--- thermometer.asm
+++ thermometer.asm
@@ -1,6 +1,6 @@
 ; this short program for emu8086 shows how to keep constant temperature
-; using heater and thermometer (between 60° to 80°),
-; it is assumed that air temperature is lower 60°.
+; using heater and thermometer (between 60? to 80?),
+; it is assumed that air temperature is lower 60?.
  
 ; thermometer.exe is started automatically from c:\emu8086\devices.
 ; it is also accessible from the "virtual devices" menu of the emulator.
@@ -15,13 +15,120 @@
 
 name "thermo"
 
-TEMP_LOW     equ 60
-TEMP_HIGH    equ 80
-PORT_THERMO  equ 125
-PORT_HEATER  equ 127
-ERROR_FLAG   equ 1;
-
 start:
+     mov ah,01h
+     int 21h  
+     sub al,30h
+     mov byte1[0],al
+         
+     mov ah,01h
+     int 21h
+     cmp al,13
+     je suma
+     sub al,30h 
+     mov byte2[0],al 
+     
+     mov ah,01h
+     int 21h
+     cmp al,13
+     je suma               
+     sub al,30h
+     mov byte3[0],al
+        
+     mov dl,0
+     suma:    
+     operar_con_unidad_low:
+     cmp byte2[0],0
+     jne operar_con_decena_low
+     add dl,byte1[0]
+     jmp  asignar_temp_low
+     
+     
+     operar_con_decena_low:
+     cmp byte3[0],0
+     jne operar_con_centena_low
+     mov ax,0
+     mov al,byte1[0]
+     mov bl,10
+     mul bl
+     add al,byte2[0]  
+     mov dl,al
+     jmp asignar_temp_low
+      
+     
+     operar_con_centena_low:
+     mov ax,0
+     mov al,byte1[0]
+     mov bl,100
+     mul bl
+     mov dl,al
+     mov al,byte2[0]
+     mov bl,10
+     mul bl
+     add dl,al
+     add dl,byte3[0]  
+     jmp asignar_temp_low
+     
+     asignar_temp_low:
+     mov TEMP_LOW[0], dl
+     
+     mov ah,01h
+     int 21h  
+     sub al,30h
+     mov byte1[0],al
+         
+     mov ah,01h
+     int 21h
+     cmp al,13
+     je suma2
+     sub al,30h 
+     mov byte2[0],al 
+     
+     mov ah,01h
+     int 21h
+     cmp al,13
+     je suma2              
+     sub al,30h
+     mov byte3[0],al
+        
+     
+     suma2:    
+     operar_con_unidad_high:
+     mov dl,0
+     cmp byte2[0],0
+     jne operar_con_decena_high
+     add dl,byte1[0]
+     jmp  asignar_temp_high
+     
+     
+     operar_con_decena_high:
+     cmp byte3[0],0
+     jne operar_con_centena_high
+     mov ax,0
+     mov al,byte1[0]
+     mov bl,10
+     mul bl
+     add al,byte2[0]  
+     mov dl,al
+     jmp asignar_temp_high
+      
+     
+     operar_con_centena_high:
+     mov ax,0
+     mov al,byte1[0]
+     mov bl,100
+     mul bl
+     mov dl,al
+     mov al,byte2[0]
+     mov bl,10
+     mul bl
+     add dl,al
+     add dl,byte3[0]  
+     jmp asignar_temp_high
+     
+     asignar_temp_high:
+     mov TEMP_HIGH[0], dl  
+     
     ; Read temperature from thermometer
     in al, PORT_THERMO
     
@@ -40,7 +147,7 @@ start:
 
     cmp al, TEMP_HIGH
     jle turnHeaterOff
-    jmp turnHeaterOn  ; If temperature exceeds 80°, turn on heater
+    jmp turnHeaterOn  ; If temperature exceeds 80?, turn on heater
 
 turnHeaterOn:
     mov al, 1
@@ -61,3 +168,12 @@ error_handler:
     mov al, ERROR_FLAG  ; Error flag
     jmp continue
 
+
+TEMP_LOW     db 0
+TEMP_HIGH    db 0;reservo 100 bites y lo q no use son "?
+PORT_THERMO  equ 125
+PORT_HEATER  equ 127
+ERROR_FLAG   equ 1;   
+byte1 db 0
+byte2 db 0
+byte3 db 0
